3d06e10c79d917f4f2df058639ec6167283af68f
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-07-02 00:36:45 +0300

    init: init project; feat: add the stranger things main theme

---
+++ TheStrangerThings/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//октава
+int octave = 2;
+int duration = 200;
+int[] notes = { 130, 164, 196, 246, 261, 246, 196, 164 }; //до ми соль си до си соль ми =)
+int i = 0;
+
+while(true)
+{
+    Console.Beep(notes[i] * octave, duration);
+    i++;
+    if (i == notes.Length) i = 0;
+}
